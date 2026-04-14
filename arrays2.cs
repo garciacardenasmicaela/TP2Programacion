92c6e0f5e7b322b09a6c84b5b922b4c5919fba8a
Micaela Garcia Cardenas <garciacardenasmicaela@gmail.com>
2026-04-14 15:10:17 -0300

    Ejercicio 2 Arrays: Arrays y Foreach() (5 ejercicios en el archivo)

--- arrays2.cs
+++ arrays2.cs
@@ -1,16 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TP2ProgramacionIII
+﻿namespace TP2ProgramacionIII
 {
     internal class arrays2
     {
         public static void Ejecutar()
         {
+            Console.WriteLine("Seleccione un ejercicio del 1 al 5:");
+            int opcion = int.Parse(Console.ReadLine());
 
+            switch (opcion)
+            {
+                case 1:
+                    string[] frutas = { "Manzana", "Banana", "Uva", "Naranja" };
+                    Console.WriteLine("Ejercicio 1");
+                    foreach (string f in frutas)
+                    {
+                        Console.WriteLine(f);
+                    }
+                    break;
+                case 2:
+                    string[] colores = { "Rojo", "Azul", "Verde" };
+                    Console.WriteLine("Ejercicio 2");
+                    foreach (string c in colores)
+                    {
+                        Console.WriteLine(c);
+                    }
+                    break;
+
+                case 3:
+                    string[] paises = { "Argentina", "Brasil", "Chile", "Peru" };
+                    Console.WriteLine("Ejercicio 3");
+                    foreach (string p in paises)
+                    {
+                        Console.WriteLine(p);
+                    }
+                    break;
+
+                case 4:
+                    string[] animales = { "Perro", "Gato", "Caballo" };
+                    Console.WriteLine("Ejercicio 4");
+                    foreach (string a in animales)
+                    {
+                        Console.WriteLine(a);
+                    }
+                    break;
+
+                case 5:
+                    string[] lenguajes = { "C#", "Java", "Python", "JavaScript" };
+                    Console.WriteLine("Ejercicio 5");
+                    foreach (string l in lenguajes)
+                    {
+                        Console.WriteLine(l);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
