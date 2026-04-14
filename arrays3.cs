49da1acf50e8fa05d4c2802afddc369b81ae882b
Micaela Garcia Cardenas <garciacardenasmicaela@gmail.com>
2026-04-14 15:13:54 -0300

    Ejercicio 3 Arrays: .length y bucle FOR (5 ejercicios en cada archivo)

--- arrays3.cs
+++ arrays3.cs
@@ -1,16 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TP2ProgramacionIII
+﻿namespace TP2ProgramacionIII
 {
     internal class arrays3
     {
         public static void Ejecutar()
         {
+            Console.WriteLine("Seleccione un ejercicio del 1 al 5:");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    int[] edades = { 15, 22, 30, 18, 25 };
+                    Console.WriteLine("Ejercicio 1");
+                    for (int i = 0; i < edades.Length; i++)
+                    {
+                        Console.WriteLine("Indice " + i + ": " + edades[i]);
+                    }
+                    break;
 
+                case 2:
+                    int[] numeros = { 5, 10, 15, 20 };
+                    Console.WriteLine("Ejercicio 2");
+                    for (int i = 0; i < numeros.Length; i++)
+                    {
+                        Console.WriteLine("Indice " + i + ": " + numeros[i]);
+                    }
+                    break;
+
+                case 3:
+                    int[] sueldos = { 1000, 2000, 3000 };
+                    Console.WriteLine("Ejercicio 3");
+                    for (int i = 0; i < sueldos.Length; i++)
+                    {
+                        Console.WriteLine("Indice " + i + ": " + sueldos[i]);
+                    }
+                    break;
+
+                case 4:
+                    int[] notas = { 6, 7, 8, 9, 10 };
+                    Console.WriteLine("Ejercicio 4");
+                    for (int i = 0; i < notas.Length; i++)
+                    {
+                        Console.WriteLine("Indice " + i + ": " + notas[i]);
+                    }
+                    break;
+
+                case 5:
+                    int[] puntos = { 100, 200, 300, 400 };
+                    Console.WriteLine("Ejercicio 5");
+                    for (int i = 0; i < puntos.Length; i++)
+                    {
+                        Console.WriteLine("Indice " + i + ": " + puntos[i]);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
